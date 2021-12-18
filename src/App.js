92126054ec0cc3d954d4509789ffd56e6986c0a0
Punith K <kpunith8@gmail.com>
2021-12-18 19:18:14 +0530

    Unspash finished UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
-function App() {
+const API_URL = 'https://serverless-api.punith-tech.workers.dev/'
+
+const getImages = async (query) => {
+  const resp = await fetch(API_URL, { method: 'POST', body: JSON.stringify({ query }), headers: { 'Content-type': 'application/json' } })
+
+  return resp.json()
+}
+
+const App = () => {
+  const [query, setQuery] = useState('')
+  const [images, setImages] = useState([])
+
+  const handleSearch = async () => {
+    const result = await getImages(query)
+    setImages(result)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="query-form">
+        <input type="text" id="query" onChange={e => setQuery(e.target.value)} placeholder='Search query' value={query} />
+        <button style={{marginLeft: 10}} onClick={handleSearch}>Search</button>
+      </div>
+      <div style={{ marginTop: 50, padding: '0 100px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexWrap: 'wrap', gap: 10 }}>
+        {images.map(image => <a key={image.id} href={image.link} target="_blank" rel="noreferrer">
+          <img src={image.url} alt={image.id} height={200} width={280} />
+        </a>)}
+      </div>
+
+
     </div>
   );
 }
